8d942aa54f77c14e3f585525a44a3dfcc94fa26c
Lucas <lucasvalenteds@gmail.com>
2018-12-01 21:40:54 -0200

    Bootstrap project from start.ktor.io

---
+++ src/Application.kt
@@ -0,0 +1,119 @@
+package io.chiu
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.jackson.*
+import io.ktor.features.*
+import io.ktor.locations.*
+import org.slf4j.event.*
+import io.ktor.websocket.*
+import io.ktor.http.cio.websocket.*
+import java.time.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    install(Locations) {
+    }
+
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        header("MyCustomHeader")
+        allowCredentials = true
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+
+    // http://ktor.io/servers/features/https-redirect.html#testing
+    if (!testing) {
+        install(HttpsRedirect) {
+            // The port to redirect to. By default 443, the default HTTPS port.
+            sslPort = 443
+            // 301 Moved Permanently, or 302 Found redirect.
+            permanentRedirect = true
+        }
+    }
+
+    install(io.ktor.websocket.WebSockets) {
+        pingPeriod = Duration.ofSeconds(15)
+        timeout = Duration.ofSeconds(15)
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/json/jackson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+
+        get<MyLocation> {
+            call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
+        }
+        // Register nested routes
+        get<Type.Edit> {
+            call.respondText("Inside $it")
+        }
+        get<Type.List> {
+            call.respondText("Inside $it")
+        }
+
+        install(StatusPages) {
+            exception<AuthenticationException> { cause ->
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+            exception<AuthorizationException> { cause ->
+                call.respond(HttpStatusCode.Forbidden)
+            }
+
+        }
+
+        webSocket("/myws/echo") {
+            send(Frame.Text("Hi from server"))
+            while (true) {
+                val frame = incoming.receive()
+                if (frame is Frame.Text) {
+                    send(Frame.Text("Client said: " + frame.readText()))
+                }
+            }
+        }
+    }
+}
+
+@Location("/location/{name}")
+class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
+
+@Location("/type/{name}") data class Type(val name: String) {
+    @Location("/edit")
+    data class Edit(val type: Type)
+
+    @Location("/list/{page}")
+    data class List(val type: Type, val page: Int)
+}
+
+class AuthenticationException : RuntimeException()
+class AuthorizationException : RuntimeException()
+
