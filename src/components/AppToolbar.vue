cb0cdfc7fa95394aa528d6274f07b615255749b9
Lucas <lucasvalenteds@gmail.com>
2018-12-01 19:41:13 -0200

    Change toolbar color and accent color to pink

--- src/components/AppToolbar.vue
+++ src/components/AppToolbar.vue
@@ -1,11 +1,11 @@
 <template>
-    <v-toolbar app>
+    <v-toolbar app class="primary">
       <v-toolbar-title class="headline">
-        <span>{{ $t("app.name") }}</span>
+        <span class="white--text">{{ $t("app.name") }}</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn @click="$emit('onContribute')" flat>{{ $t("app.toolbar.contribute") }}</v-btn>
-      <v-btn @click="$emit('onAbout')" flat>{{ $t("app.toolbar.about") }}</v-btn>
+      <v-btn class="white--text" @click="$emit('onContribute')" flat>{{ $t("app.toolbar.contribute") }}</v-btn>
+      <v-btn class="white--text" @click="$emit('onAbout')" flat>{{ $t("app.toolbar.about") }}</v-btn>
     </v-toolbar>
 </template>
 
