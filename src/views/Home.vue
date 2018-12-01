041281badd33c1973ef963416b280143d3ffa7e2
Lucas <lucasvalenteds@gmail.com>
2018-12-01 20:06:52 -0200

    Refactoring Home view layout
    * Organize the UI as a grid
    * Add NoiseLevelsTable as a component to be shown

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,43 @@
 <template>
-  <div>
-    <h1>{{ $t("noise.meter.chart.title") }}</h1>
-    <NoiseMeterChart/>
-  </div>
+  <v-container fluid grid-list-xl>
+    <v-layout row wrap justify-center>
+      <v-flex xs12 sm6 lg4>
+        <v-card>
+          <v-card-title primary-title>
+            <v-icon class="mr-3">hearing</v-icon>
+            <h3 class="headline">{{ $t("noise.meter.chart.noise.title") }}</h3>
+          </v-card-title>
+          <v-divider></v-divider>
+          <NoiseMeterChart/>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm6 lg4>
+        <v-card>
+          <v-card-title primary-title>
+            <v-icon class="mr-3">help_outline</v-icon>
+            <h3 class="headline">{{ $t("noise.levels.table.title") }}</h3>
+          </v-card-title>
+          <v-divider></v-divider>
+          <NoiseLevelsTable/>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <p class="subheading pa-2">Fonte: ABTN</p>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script lang="ts">
+import NoiseLevelsTable from "@/components/NoiseLevelsTable.vue";
 import NoiseMeterChart from "@/components/NoiseMeterChart.vue";
 import Vue from "vue";
 import Component from "vue-class-component";
 
 @Component({
   components: {
+    NoiseLevelsTable,
     NoiseMeterChart,
   },
 })
