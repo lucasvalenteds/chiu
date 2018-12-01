985868dc8666e2fa4737873d6e7d58f642b20703
Lucas <lucasvalenteds@gmail.com>
2018-12-01 16:28:11 -0200

    Setup internationalization

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./plugins/vuetify";
 import router from "./router";
+import i18n from "./plugins/i18n";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  i18n,
   render: (h) => h(App),
 }).$mount("#app");
