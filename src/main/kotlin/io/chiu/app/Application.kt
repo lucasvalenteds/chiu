476d6835863f1c1be3574cf6b9cc6ad0a1ef5407
Lucas <lucasvalenteds@gmail.com>
2018-12-02 18:09:15 -0200

    Cleanup Application routing implementation

--- src/main/kotlin/io/chiu/app/Application.kt
+++ src/main/kotlin/io/chiu/app/Application.kt
@@ -10,7 +10,6 @@ import io.chiu.app.features.enableLogging
 import io.chiu.app.features.enableWebSockets
 import io.chiu.app.features.getJsonMapper
 import io.ktor.application.Application
-import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.application.log
 import io.ktor.http.ContentType
@@ -25,11 +24,10 @@ import io.ktor.server.netty.EngineMain
 import io.ktor.util.KtorExperimentalAPI
 import io.ktor.websocket.webSocket
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.channels.produce
-import kotlinx.coroutines.delay
-import java.time.LocalDateTime
-import java.util.UUID
+import kotlinx.coroutines.channels.Channel
+import org.bson.types.ObjectId
+import java.time.Instant
+import java.util.Date
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
@@ -45,60 +43,39 @@ fun Application.module() {
     })
     enableWebSockets()
 
-    val channel: ReceiveChannel<SSEEvent> = produce {
-        var n = 0
-        while (true) {
-            send(SSEEvent(n, "noise", Noise(UUID.randomUUID(), (0..120).random(), LocalDateTime.now())))
-            delay(1000)
-            n++
-        }
-    }
-
     routing {
-        get("/listen") {
-            call.respondSse(channel)
-        }
-        enableExceptionHandling()
+        val noiseChannel = Channel<NoiseEvent>()
+
         webSocket("/report") {
+            var iteration = 0
             while (true) {
-                when (val frame = incoming.receive()) {
-                    is Frame.Text -> {
-                        log.info(getJsonMapper().readValue<Report>(frame.readText()).toString())
-                        send(Frame.Text(frame.readText()))
-                    }
+                val payload = incoming.receive() as Frame.Text
+                val level = getJsonMapper().readValue<NoiseLevel>(payload.readText())
+                val report = NoiseReport(ObjectId().toString(), level.level, Date.from(Instant.now()))
+                val sseEvent = NoiseEvent(iteration, "noise", report)
+                log.info("/report :: {}", sseEvent.toString())
+                noiseChannel.send(sseEvent)
+                iteration++
+            }
+        }
+
+        get("/listen") {
+            call.response.header(HttpHeaders.CacheControl, "no-cache")
+            call.respondTextWriter(contentType = ContentType.parse("text/event-stream")) {
+                for (sseEvent in noiseChannel) {
+                    log.info("/listen :: {}", sseEvent.toString())
+                    write("id: ${sseEvent.id}\n")
+                    write("event: ${sseEvent.event}\n")
+                    write("data: ${getJsonMapper().writeValueAsString(sseEvent.data)}\n")
+                    write("\n")
+                    flush()
                 }
             }
         }
+        enableExceptionHandling()
     }
 }
 
-data class Report(
-    val latitude: Double,
-    val longitude: Double,
-    val level: Int
-)
-
-data class SSEEvent(
-    val id: Int,
-    val event: String,
-    val data: Noise
-)
-
-data class Noise(
-    val uuid: UUID,
-    val level: Int,
-    val timestamp: LocalDateTime
-)
-
-private suspend fun ApplicationCall.respondSse(events: ReceiveChannel<SSEEvent>) {
-    response.header(HttpHeaders.CacheControl, "no-cache")
-    respondTextWriter(contentType = ContentType.parse("text/event-stream")) {
-        for (event in events) {
-            write("id: ${event.id}\n")
-            write("event: ${event.event}\n")
-            write("data: ${getJsonMapper().writeValueAsString(event.data)}\n")
-            write("\n")
-            flush()
-        }
-    }
-}
+data class NoiseLevel(val level: Int)
+data class NoiseReport(val id: String, val level: Int, val timestamp: Date)
+data class NoiseEvent(val id: Int, val event: String, val data: NoiseReport)
