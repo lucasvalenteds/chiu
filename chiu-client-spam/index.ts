b88cf015d15a6bfbd2dad298fab8cd8efcb5bd78
Lucas <lucasvalenteds@gmail.com>
2018-12-05 20:37:57 -0200

    Move all files to folder `chiu-client-spam`

---
+++ chiu-client-spam/index.ts
@@ -0,0 +1,40 @@
+import { client as WebSocket, connection, IMessage } from "websocket";
+import EventSource from "eventsource";
+
+//const address: string = "chiu-app.herokuapp.com";
+const address: string = "localhost:8080";
+
+// SENDING DATA
+const socket = new WebSocket();
+socket.on("connect", (ws: connection) => {
+    ws.on("close", (code: number, description: string) => {
+        console.log("Disconnected from /report: ", code, description);
+    });
+
+    ws.on("error", (error: Error) => {
+        console.error("Error from /report: ", error.message);
+    });
+
+    ws.addListener("message", (data: IMessage) => {
+        console.log("Message from /report: ", data.utf8Data);
+    });
+
+    setInterval(() => ws.sendUTF(JSON.stringify({
+        level: Math.floor(Math.random() * 100)
+    })), 100);
+});
+socket.connect(`ws://${address}/report`);
+
+// RECEIVING DATA
+const sse = new EventSource(`http://${address}/listen`);
+sse.addEventListener("close", (event: any) => {
+    console.log("Disconnected from /listen: ", event);
+});
+
+sse.addEventListener("error", (event: any) => {
+    console.log("Error from /listen: ", event);
+})
+
+sse.addEventListener("noise", (event: any) => {
+    console.log("Message from /listen: ", event.data);
+});
