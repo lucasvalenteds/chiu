a5eb80e16d22df7ebafb41a097547aaec5f56b80
Lucas <lucasvalenteds@gmail.com>
2018-12-05 18:33:22 -0200

    Setup Ethernet connection

--- main.ino
+++ main.ino
@@ -1,8 +1,17 @@
+#include <Ethernet.h>
 #include <ArduinoJson.h>
+    
+EthernetClient ethernetClient;
 
 void setup()
 {
     Serial.begin(9600);
+
+    // Setup internet connection
+    byte arduinoIpAddress[] = { 10, 0, 0, 177 };
+    byte arduinoMacAddress[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+    Ethernet.begin(arduinoMacAddress, arduinoIpAddress);
+    Serial.println(Ethernet.linkStatus());
 
     // Create the request payload in JSON
     const size_t bufferSize = JSON_OBJECT_SIZE(1);
