e439b9340a61d910761d0d5044f33e26324d58d1
Lucas <lucasvalenteds@gmail.com>
2018-12-05 23:05:30 -0200

    arduino: Remove comments from `main.ino`

--- chiu-client-arduino/main.ino
+++ chiu-client-arduino/main.ino
@@ -5,25 +5,20 @@
 #include <Ethernet.h>
 #include <ArduinoJson.h>
 
-// Setup internet connection
 EthernetClient ethernetClient;
 
-// Setup server connection
 char serverIp[] = "192.168.1.8";
 int serverPort = 8080;
 WebSocketClient client = WebSocketClient(ethernetClient, serverIp, serverPort);
 
-// Setup JSON object that will be sent to server
 const size_t bufferSize = JSON_OBJECT_SIZE(1);
 DynamicJsonBuffer jsonBuffer(bufferSize);
 JsonObject &payloadJson = jsonBuffer.createObject();
 
 void setup()
 {
-    // Setup Arduino connection
     Serial.begin(9600);
 
-    // Setup internet connection
     byte arduinoIpAddress[] = {192, 168, 1, 177};
     byte arduinoMacAddress[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
     Ethernet.begin(arduinoMacAddress, arduinoIpAddress);
@@ -35,19 +30,15 @@ void loop()
 
     while (client.connected())
     {
-        // Serialize the current noise level as JSON
         payloadJson["level"].set(random(120));
 
-        // Convert the JSON object to a String object
         String payload;
         payloadJson.printTo(payload);
 
-        // Send the String object to the server
         client.beginMessage(TYPE_TEXT);
         client.print(payload);
         client.endMessage();
 
-        // Wait some time before sending data to server again
         delay(500);
     }
 }
