ff114853dd0e963e4662d07bd8a0ad42be9939fe
Lucas <lucasvalenteds@gmail.com>
2018-12-14 03:33:36 -0200

    backend: Add test to DatabaseNoSQL

---
+++ chiu-backend/src/test/kotlin/io/chiu/configuration/DatabaseNoSQLTest.kt
@@ -0,0 +1,37 @@
+package io.chiu.configuration
+
+import com.mongodb.reactivestreams.client.MongoCollection
+import io.chiu.app.NoiseReport
+import io.chiu.app.configuration.Database
+import io.chiu.app.configuration.DatabaseNoSQL
+import io.kotlintest.specs.FeatureSpec
+import io.mockk.mockk
+import io.mockk.spyk
+import io.mockk.verify
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeoutOrNull
+import org.bson.Document
+import java.time.Instant
+import java.util.Date
+
+class DatabaseNoSQLTest : FeatureSpec({
+
+    val collection: MongoCollection<Document> = mockk(relaxed = true)
+    val repository: Database = DatabaseNoSQL(collection)
+
+    feature("Saving noises") {
+        scenario("It persists _id, level and timestamps") {
+            runBlocking {
+                val report = spyk(NoiseReport("123", 10, Date.from(Instant.now())))
+
+                withTimeoutOrNull(1000) {
+                    repository.saveNoiseReport(report)
+                }
+
+                verify(exactly = 1) { report.id }
+                verify(exactly = 1) { report.level }
+                verify(exactly = 1) { report.timestamp }
+            }
+        }
+    }
+})
