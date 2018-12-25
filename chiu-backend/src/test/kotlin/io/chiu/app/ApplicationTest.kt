a32975d1b380ebb7597c7febae88a772ae68ece4
Lucas <lucasvalenteds@gmail.com>
2018-12-25 19:06:23 -0200

    backend: Fixes test case compilation error

--- chiu-backend/src/test/kotlin/io/chiu/app/ApplicationTest.kt
+++ chiu-backend/src/test/kotlin/io/chiu/app/ApplicationTest.kt
@@ -6,43 +6,39 @@ import io.chiu.app.testing.testableModule
 import io.kotlintest.fail
 import io.kotlintest.specs.StringSpec
 import io.ktor.http.cio.websocket.Frame
+import io.ktor.http.cio.websocket.readText
 import io.ktor.server.testing.withTestApplication
 import io.ktor.util.KtorExperimentalAPI
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import strikt.api.expectThat
-import strikt.assertions.isNotNull
+import strikt.assertions.isEqualTo
 
 @ObsoleteCoroutinesApi
 @ExperimentalCoroutinesApi
 @KtorExperimentalAPI
-@Suppress("UNREACHABLE_CODE", "UNUSED_VARIABLE")
 class ApplicationTest : StringSpec({
 
     val database = mockk<Database>(relaxed = true)
-    val noiseChannel = Channel<NoiseEvent>()
+    val noiseChannel = BroadcastChannel<NoiseEvent>(Channel.CONFLATED)
     val noise = NoiseLevel(level = (0..150).random())
 
     "SSE /listen streams the current noise level" {
         fail("No idea how to test SSE for the current implementation")
     }
     "WS / persists the current noise level" {
-        fail("Test hanging for unknown reason")
-
         withTestApplication({ testableModule(database, noiseChannel) }) {
-            launch {
-                handleWebSocketConversation("/") { _, outgoing ->
-                    val level = JSON.writeValueAsString(noise)
-                    outgoing.send(Frame.Text(level))
-                }
-            }
-            runBlocking {
-                expectThat(noiseChannel.receiveOrNull()).isNotNull()
+            handleWebSocketConversation("/") { incoming, outgoing ->
+                val level = JSON.writeValueAsString(noise)
+                outgoing.send(Frame.Text(level))
+
+                val responseMessage = (incoming.receive() as Frame.Text).readText()
+
+                expectThat(responseMessage).isEqualTo("""{"status":"OK"}""")
                 coVerify(exactly = 1) { database.saveNoiseReport(any()) }
             }
         }
