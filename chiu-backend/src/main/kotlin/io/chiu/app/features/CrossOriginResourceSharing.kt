7e132dbf8ad3e913abe5cedc1d09c8de970ed012
Lucas <lucasvalenteds@gmail.com>
2018-12-25 22:29:33 -0200

    backend: Remove unused imports

--- chiu-backend/src/main/kotlin/io/chiu/app/features/CrossOriginResourceSharing.kt
+++ chiu-backend/src/main/kotlin/io/chiu/app/features/CrossOriginResourceSharing.kt
@@ -3,8 +3,6 @@ package io.chiu.app.features
 import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.features.CORS
-import io.ktor.http.HttpHeaders
-import io.ktor.http.HttpMethod
 
 fun Application.enableCORS() {
     install(CORS) {
