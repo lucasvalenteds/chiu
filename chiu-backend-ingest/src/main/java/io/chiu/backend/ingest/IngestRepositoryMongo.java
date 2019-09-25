d9f048a3458230dde8de95663cedf8aec0edf0a2
Lucas <lucasvalenteds@gmail.com>
2019-09-25 17:49:37 -0300

    Implement MongoDB repository

---
+++ chiu-backend-ingest/src/main/java/io/chiu/backend/ingest/IngestRepositoryMongo.java
@@ -0,0 +1,36 @@
+package io.chiu.backend.ingest;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.Success;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bson.Document;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Mono;
+
+public class IngestRepositoryMongo implements IngestRepository {
+
+    private static final Logger log = LogManager.getLogger(IngestRepositoryMongo.class);
+
+    private final MongoClient client;
+
+    public IngestRepositoryMongo(MongoClient client) {
+        this.client = client;
+    }
+
+    @Override
+    public Mono<Void> save(SensorData data) {
+        Document document = new Document()
+            .append("_id", data.getId())
+            .append("level", data.getLevel());
+
+        Publisher<Success> result = client.getDatabase("chiu")
+            .getCollection("noise")
+            .insertOne(document);
+
+        return Mono.from(result)
+            .doOnNext(log::info)
+            .doOnError(log::error)
+            .thenEmpty(Mono.empty());
+    }
+}
---
+++ chiu-backend-ingest/src/test/java/io/chiu/backend/ingest/IngestRepositoryMongoTest.java
@@ -0,0 +1,37 @@
+package io.chiu.backend.ingest;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoCollection;
+import com.mongodb.reactivestreams.client.MongoDatabase;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.reactivestreams.Subscriber;
+import reactor.test.StepVerifier;
+
+class IngestRepositoryMongoTest {
+
+    private final MongoClient client = Mockito.mock(MongoClient.class);
+    private final MongoDatabase database = Mockito.mock(MongoDatabase.class);
+    private final MongoCollection collection = Mockito.mock(MongoCollection.class);
+
+    private final IngestRepository repository = new IngestRepositoryMongo(client);
+
+    @Test
+    void testItCanPersistSensorData() {
+        SensorData sensorData = Mockito.mock(SensorData.class);
+
+        Mockito.when(client.getDatabase("chiu")).thenReturn(database);
+        Mockito.when(database.getCollection("noise")).thenReturn(collection);
+        Mockito.when(collection.insertOne(Mockito.any())).thenReturn(Subscriber::onComplete);
+        Mockito.when(sensorData.getId()).thenReturn(UUID.randomUUID());
+        Mockito.when(sensorData.getLevel()).thenReturn(65);
+
+        StepVerifier.create(repository.save(sensorData))
+            .expectComplete()
+            .verify();
+
+        Mockito.verify(sensorData, Mockito.times(1)).getId();
+        Mockito.verify(sensorData, Mockito.times(1)).getLevel();
+    }
+}
