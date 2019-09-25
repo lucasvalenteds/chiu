fadf2b8fa1e4ddb3853656d0ec43780194a90a0f
Lucas <lucasvalenteds@gmail.com>
2019-09-25 14:44:40 -0300

    Extract server port to properties file

--- chiu-backend-ingest/src/main/java/io/chiu/backend/ingest/AppConfiguration.java
+++ chiu-backend-ingest/src/main/java/io/chiu/backend/ingest/AppConfiguration.java
@@ -1,13 +1,21 @@
 package io.chiu.backend.ingest;
 
 import java.util.function.Consumer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import reactor.netty.http.server.HttpServer;
 import reactor.netty.http.server.HttpServerRoutes;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 class AppConfiguration {
+
+    @Autowired
+    private Environment environment;
 
     @Bean
     Consumer<HttpServerRoutes> router() {
@@ -18,7 +26,7 @@ class AppConfiguration {
     @Bean
     HttpServer server(Consumer<HttpServerRoutes> router) {
         return HttpServer.create()
-            .port(8080)
+            .port(environment.getProperty("server.port", Integer.class, 8080))
             .route(router);
     }
 }
