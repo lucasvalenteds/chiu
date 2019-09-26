6d3b59d9bdcf8a94181c08b01dd008bf64e61069
Lucas <lucasvalenteds@gmail.com>
2019-09-26 14:52:10 -0300

    Remove `chiu-backend`

--- chiu-backend/src/main/kotlin/io/chiu/app/features/WebSocket.kt
+++
@@ -1,9 +0,0 @@
-package io.chiu.app.features
-
-import io.ktor.application.Application
-import io.ktor.application.install
-import io.ktor.websocket.WebSockets
-
-fun Application.enableWebSockets() {
-    install(WebSockets)
-}
--- chiu-backend/src/test/kotlin/io/chiu/app/testing/InMemoryDatabase.kt
+++
@@ -1,8 +0,0 @@
-package io.chiu.app.testing
-
-import io.chiu.app.NoiseReport
-import io.chiu.app.configuration.Database
-
-class InMemoryDatabase : Database {
-    override suspend fun saveNoiseReport(report: NoiseReport) = Unit
-}
