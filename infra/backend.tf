6ac61154b2c52e226ea9c0a3a31f2fcb26825221
Lucas <lucasvalenteds@gmail.com>
2019-09-30 09:19:20 -0300

    Add Terraform script to deploy backend to Heroku

---
+++ infra/backend.tf
@@ -0,0 +1,38 @@
+variable "heroku_email" {}
+variable "heroku_api_key" {}
+variable "heroku_app_name" {}
+variable "chiu_frontend_url" {}
+
+provider "heroku" {
+  version = "~> 2.0"
+  email   = "${var.heroku_email}"
+  api_key = "${var.heroku_api_key}"
+}
+
+resource "heroku_app" "chiu_backend" {
+  name   = "${var.heroku_app_name}"
+  region = "us"
+
+  config_vars = {
+    FRONTEND_URL = "${var.chiu_frontend_url}"
+    GRADLE_TASK = "shadowJar"
+  }
+}
+
+resource "heroku_build" "chiu_backend" {
+  app = "${heroku_app.chiu_backend.name}"
+  buildpacks = ["https://github.com/heroku/heroku-buildpack-gradle.git"]
+
+  source = {
+    path = "../chiu-backend"
+  }
+}
+
+resource "heroku_addon" "chiu_database" {
+  app  = "${heroku_app.chiu_backend.name}"
+  plan = "mongolab:sandbox"
+}
+
+output "service_url" {
+  value = "https://${heroku_app.chiu_backend.name}.herokuapp.com"
+}
