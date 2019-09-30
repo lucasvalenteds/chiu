bb37cdf1da5239e43cc6702299beb8dcde119556
Lucas <lucasvalenteds@gmail.com>
2019-09-30 11:49:05 -0300

    Organizing Terraform scripts
    Move files from `infra/` folder to backend and frontend folders.

---
+++ chiu-frontend/main.tf
@@ -0,0 +1,29 @@
+variable "heroku_email" {}
+variable "heroku_api_key" {}
+variable "heroku_app_name" {}
+
+provider "heroku" {
+  version = "~> 2.0"
+  email   = "${var.heroku_email}"
+  api_key = "${var.heroku_api_key}"
+}
+
+resource "heroku_app" "chiu_frontend" {
+  name   = "${var.heroku_app_name}"
+  region = "us"
+}
+
+resource "heroku_build" "chiu_frontend" {
+  app = "${heroku_app.chiu_frontend.name}"
+  buildpacks = [
+    "https://github.com/heroku/heroku-buildpack-nodejs.git"
+  ]
+
+  source = {
+    path = "./"
+  }
+}
+
+output "service_url" {
+  value = "https://${heroku_app.chiu_frontend.name}.herokuapp.com"
+}
