lucasvalenteds/chiu

commit ccbfb1b7b8f6ef024123f4e417218a8c8f733699

Split project into multiple Gradle projects

chiu-backend/chiu-testing-integration/build.gradle.kts
@@ -1,0 +1,26 @@
+plugins {
+    java
+    application
+}
+
+dependencies {
+    implementation(project(":chiu-server"))
+    implementation("io.projectreactor", "reactor-core", properties["version.reactor"].toString())
+    testImplementation("io.projectreactor", "reactor-test", properties["version.reactor"].toString())
+    implementation("io.projectreactor.netty", "reactor-netty", properties["version.netty"].toString())
+    implementation("org.springframework", "spring-context", properties["version.spring"].toString())
+    testImplementation("org.springframework", "spring-test", properties["version.spring"].toString())
+    implementation("com.fasterxml.jackson.core", "jackson-databind", properties["version.jackson"].toString())
+    implementation("com.github.fge", "throwing-lambdas", properties["version.throwing"].toString())
+    implementation("com.devskiller", "jfairy", properties["version.jfairy"].toString())
+}
+
+tasks.register<JavaExec>("runConsumer") {
+    classpath = sourceSets["main"].runtimeClasspath
+    main = "io.chiu.backend.ExampleConsumer"
+}
+
+tasks.register<JavaExec>("runProducer") {
+    classpath = sourceSets["main"].runtimeClasspath
+    main = "io.chiu.backend.ExampleProducer"
+}

chiu-backend/build.gradle.kts
@@ -1,107 +1,32 @@
-import com.github.lkishalmi.gradle.gatling.GatlingPluginExtension
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     java
-    application
-    scala
-    id("com.github.lkishalmi.gatling") version "3.2.9"
-    id("com.github.johnrengelman.shadow") version "5.1.0"
 }
 
-repositories {
-    jcenter()
-}
+subprojects {
+    apply(plugin = "java")
 
-dependencies {
-    implementation("io.projectreactor", "reactor-core", properties["version.reactor"].toString())
-    testImplementation("io.projectreactor", "reactor-test", properties["version.reactor"].toString())
-    implementation("io.projectreactor.netty", "reactor-netty", properties["version.netty"].toString())
-    implementation("org.springframework", "spring-context", properties["version.spring"].toString())
-    testImplementation("org.springframework", "spring-test", properties["version.spring"].toString())
-    implementation("io.lettuce", "lettuce-core", properties["version.redis"].toString())
-
-    implementation("com.fasterxml.jackson.core", "jackson-databind", properties["version.jackson"].toString())
-    implementation("com.github.fge", "throwing-lambdas", properties["version.throwing"].toString())
-
-    implementation("org.apache.logging.log4j", "log4j-api", properties["version.log4j"].toString())
-    implementation("org.apache.logging.log4j", "log4j-core", properties["version.log4j"].toString())
-    implementation("org.slf4j", "slf4j-simple", properties["version.slf4j"].toString())
-
-    testImplementation("com.devskiller", "jfairy", properties["version.jfairy"].toString())
-    testImplementation("org.junit.jupiter", "junit-jupiter", properties["version.junit"].toString())
-    testImplementation("org.mockito", "mockito-core", properties["version.mockito"].toString())
-
-    implementation("org.scala-lang", "scala-library", properties["version.scala"].toString())
-    gatling("io.netty", "netty-tcnative-boringssl-static", properties["version.netty.ssl"].toString())
-    gatling("io.gatling", "gatling-app", properties["version.gatling"].toString())
-    gatling("io.gatling", "gatling-core", properties["version.gatling"].toString())
-    gatling("io.gatling", "gatling-http", properties["version.gatling"].toString())
-    gatling("io.gatling.highcharts", "gatling-charts-highcharts", properties["version.gatling"].toString())
-}
-
-configure<ApplicationPluginConvention> {
-    mainClassName = "io.chiu.backend.Main"
-}
-
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
-
-configure<GatlingPluginExtension> {
-    jvmArgs = listOf(
-            "-server", "-Xmx1G",
-            "-XX:InitiatingHeapOccupancyPercent=75",
-            "-XX:+ParallelRefProcEnabled",
-            "-XX:+PerfDisableSharedMem",
-            "-XX:+OptimizeStringConcat",
-            "-XX:+HeapDumpOnOutOfMemoryError",
-            "-Djava.net.preferIPv4Stack=true",
-            "-Djava.net.preferIPv6Addresses=false"
-    )
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-    testLogging {
-        events = setOf(
-                TestLogEvent.PASSED,
-                TestLogEvent.FAILED,
-                TestLogEvent.SKIPPED,
-                TestLogEvent.STANDARD_ERROR
-        )
-    }
-}
-
-val integrationTest: SourceSet by sourceSets.creating {
-    compileClasspath += sourceSets[sourceSets.main.name].output
-}
-
-configurations {
-    getByName(integrationTest.name + "Implementation") {
-        extendsFrom(configurations.testImplementation.get())
+    repositories {
+        jcenter()
     }
 
-    getByName(integrationTest.name + "RuntimeOnly") {
-        extendsFrom(configurations.testRuntime.get())
+    dependencies {
+        implementation("org.apache.logging.log4j", "log4j-api", properties["version.log4j"].toString())
+        implementation("org.apache.logging.log4j", "log4j-core", properties["version.log4j"].toString())
+        implementation("org.slf4j", "slf4j-simple", properties["version.slf4j"].toString())
+        testImplementation("org.junit.jupiter", "junit-jupiter", properties["version.junit"].toString())
     }
-}
 
-tasks.register<Test>("itest") {
-    testClassesDirs = sourceSets[integrationTest.name].output.classesDirs
-    classpath += sourceSets[integrationTest.name].runtimeClasspath
-    systemProperty("junit.jupiter.execution.parallel.enabled", true);
-}
+    configure<JavaPluginConvention> {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 
-tasks.register<JavaExec>("runConsumer") {
-    main = "io.chiu.backend.ExampleConsumer"
-    classpath += integrationTest.runtimeClasspath
-    classpath += sourceSets["main"].runtimeClasspath
-}
-
-tasks.register<JavaExec>("runProducer") {
-    main = "io.chiu.backend.ExampleProducer"
-    classpath += integrationTest.runtimeClasspath
-    classpath += sourceSets["main"].runtimeClasspath
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            events = setOf(TestLogEvent.PASSED, TestLogEvent.FAILED, TestLogEvent.SKIPPED, TestLogEvent.STANDARD_ERROR)
+        }
+    }
 }

chiu-backend/chiu-server/build.gradle.kts
@@ -1,0 +1,22 @@
+plugins {
+    java
+    application
+    id("com.github.johnrengelman.shadow") version "5.1.0"
+}
+
+dependencies {
+    implementation("io.projectreactor", "reactor-core", properties["version.reactor"].toString())
+    testImplementation("io.projectreactor", "reactor-test", properties["version.reactor"].toString())
+    implementation("io.projectreactor.netty", "reactor-netty", properties["version.netty"].toString())
+    implementation("org.springframework", "spring-context", properties["version.spring"].toString())
+    testImplementation("org.springframework", "spring-test", properties["version.spring"].toString())
+    implementation("io.lettuce", "lettuce-core", properties["version.redis"].toString())
+    implementation("com.fasterxml.jackson.core", "jackson-databind", properties["version.jackson"].toString())
+    implementation("com.github.fge", "throwing-lambdas", properties["version.throwing"].toString())
+    testImplementation("com.devskiller", "jfairy", properties["version.jfairy"].toString())
+    testImplementation("org.mockito", "mockito-core", properties["version.mockito"].toString())
+}
+
+configure<ApplicationPluginConvention> {
+    mainClassName = "io.chiu.backend.Main"
+}

chiu-backend/chiu-testing-stress/build.gradle.kts
@@ -1,0 +1,29 @@
+import com.github.lkishalmi.gradle.gatling.GatlingPluginExtension
+
+plugins {
+    java
+    scala
+    id("com.github.lkishalmi.gatling") version "3.2.9"
+}
+
+dependencies {
+    implementation("org.scala-lang", "scala-library", properties["version.scala"].toString())
+    gatling("io.netty", "netty-tcnative-boringssl-static", properties["version.netty.ssl"].toString())
+    gatling("io.gatling", "gatling-app", properties["version.gatling"].toString())
+    gatling("io.gatling", "gatling-core", properties["version.gatling"].toString())
+    gatling("io.gatling", "gatling-http", properties["version.gatling"].toString())
+    gatling("io.gatling.highcharts", "gatling-charts-highcharts", properties["version.gatling"].toString())
+}
+
+configure<GatlingPluginExtension> {
+    jvmArgs = listOf(
+            "-server", "-Xmx1G",
+            "-XX:InitiatingHeapOccupancyPercent=75",
+            "-XX:+ParallelRefProcEnabled",
+            "-XX:+PerfDisableSharedMem",
+            "-XX:+OptimizeStringConcat",
+            "-XX:+HeapDumpOnOutOfMemoryError",
+            "-Djava.net.preferIPv4Stack=true",
+            "-Djava.net.preferIPv6Addresses=false"
+    )
+}

commit f0f6bf399021e65dc29053dfd90ca96c5b987e0e

Add Gradle task to run only integration tests
It's a fix for a regression from
ccbfb1b7b8f6ef024123f4e417218a8c8f733699.

chiu-backend/chiu-testing-integration/build.gradle.kts
@@ -15,6 +15,17 @@ dependencies {
     implementation("com.devskiller", "jfairy", properties["version.jfairy"].toString())
 }
 
+tasks.withType<Test> {
+    onlyIf {
+        systemProperties["integration"] == "yes"
+    }
+}
+
+tasks.register<Test>("itest") {
+    systemProperties["integration"] = "yes"
+    finalizedBy("test")
+}
+
 tasks.register<JavaExec>("runConsumer") {
     classpath = sourceSets["main"].runtimeClasspath
     main = "io.chiu.backend.ExampleConsumer"