28b0b39473fe3646c960cf12c3527d0ff812c406
Lucas <lucasvalenteds@gmail.com>
2019-10-15 14:37:30 -0300

    Implement handler to return latest noise level

---
+++ chiu-integration-alexa/index.js
@@ -0,0 +1,23 @@
+const Alexa = require("ask-sdk-core");
+const ChiuAPI = require("./api.js");
+
+const CurrentNoiseLevelHandler = {
+    canHandle(handlerInput) {
+        const type = Alexa.getRequestType(handlerInput.requestEnvelope);
+        const name = Alexa.getIntentName(handlerInput.requestEnvelope);
+
+        return (name === "GetNoiseLevelIntent") && (type === "IntentRequest" || type === "LaunchRequest");
+    },
+    async handle(handlerInput) {
+        const noiseLevel = await ChiuAPI.getNoiseLevel(process.env.API_URL);
+
+        return handlerInput.responseBuilder
+            .speak("O nível de ruído atual é " + noiseLevel)
+            .getResponse();
+    },
+};
+
+exports.handler = Alexa.SkillBuilders
+    .custom()
+    .addRequestHandlers(CurrentNoiseLevelHandler)
+    .lambda();
