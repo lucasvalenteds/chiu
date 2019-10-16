8656eede0e58691548c0fe2b3bed56bbd9e9b5f7
Lucas <lucasvalenteds@gmail.com>
2019-10-16 13:54:23 -0300

    Refactoring script with handlers
    * Rewrite all handlers using a class-based approach
    * Add logging to every method of every handler
    * Replace all speak commands with a simple card for debugging
    * Create function to construct the stream handdler
    * Create generic error handler

--- chiu-integration-alexa/index.ts
+++ chiu-integration-alexa/index.ts
@@ -1,27 +1,108 @@
-import { RequestHandler, HandlerInput, SkillBuilders, getIntentName } from "ask-sdk-core";
+import { RequestHandler, HandlerInput, SkillBuilders, getIntentName, ErrorHandler, getRequestType } from "ask-sdk-core";
+import { LambdaHandler } from "ask-sdk-core/dist/skill/factory/BaseSkillFactory";
 import { Response } from "ask-sdk-model";
 import * as ChiuAPI from "./api";
 
-const CurrentNoiseLevelHandler: RequestHandler = {
-    canHandle(handlerInput: HandlerInput): boolean {
-        return getIntentName(handlerInput.requestEnvelope) === process.env.AWS_INTENT_NAME;
-    },
-    async handle(handlerInput: HandlerInput): Promise<Response> {
+// alexa, qual é o nivel de ruido do projeto chiu
+export class CurrentNoiseLevelHandler implements RequestHandler {
+    async canHandle(input: HandlerInput): Promise<boolean> {
+        const isIntent = getRequestType(input.requestEnvelope) === "IntentRequest";
+        const hasExpectedName = getIntentName(input.requestEnvelope) === process.env.AWS_INTENT_NAME;
+        const result = isIntent && hasExpectedName;
+
+        console.log(CurrentNoiseLevelHandler.name + "::canHandle::isIntent:", isIntent);
+        console.log(CurrentNoiseLevelHandler.name + "::canHandle::hasExpectedName: ", hasExpectedName);
+        console.log(CurrentNoiseLevelHandler.name + "::canHandle::result:", result);
+
+        return result;
+    }
+
+    async handle(input: HandlerInput): Promise<Response> {
+        console.log(CurrentNoiseLevelHandler.name + "::handle: Request envelope:", input.requestEnvelope);
+
         try {
             const noiseLevel: string = await ChiuAPI.getNoiseLevel(process.env.CHIU_API_URL);
+            console.log(CurrentNoiseLevelHandler.name + "::handle: Noise level retrieved from server:", noiseLevel);
 
-            return handlerInput.responseBuilder
-                .speak("O nível de ruído atual é " + noiseLevel + ".")
+            return input.responseBuilder
+                .withSimpleCard("Projeto Chiu", "O nível de ruído atual é " + noiseLevel + ".")
                 .getResponse();
         } catch (error) {
-            return handlerInput.responseBuilder
-                .speak("Não foi possível obter o nível de ruído atual.")
+            console.error(CurrentNoiseLevelHandler.name + "::handle: Chiu API seems not to be available: " + error.message)
+            return input.responseBuilder
+                .withSimpleCard("Projeto Chiu", "Não foi possível obter o nível de ruído atual.")
                 .getResponse();
         }
-    },
+    }
 };
 
-exports.handler = SkillBuilders
-    .custom()
-    .addRequestHandlers(CurrentNoiseLevelHandler)
-    .lambda();
+export class LaunchRequestIntent implements RequestHandler {
+    async canHandle(input: HandlerInput): Promise<boolean> {
+        const isIntent = getRequestType(input.requestEnvelope) === "LaunchRequest";
+        console.log(LaunchRequestIntent.name + "::canHandle::isIntent:", isIntent);
+
+        return
+    }
+
+    async handle(input: HandlerInput): Promise<Response> {
+        console.log(LaunchRequestIntent.name + "::handle: Request envelope:", input.requestEnvelope);
+
+        return input.responseBuilder
+            .withSimpleCard("Projeto Chiu", "Olá do projeto Chiu :)")
+            .addDelegateDirective({
+                name: process.env.AWS_INTENT_NAME,
+                slots: {},
+                confirmationStatus: "CONFIRMED",
+            })
+            .getResponse();
+    }
+}
+
+export class SessionEndedRequest implements RequestHandler {
+    async canHandle(input: HandlerInput): Promise<boolean> {
+        const isIntent = getRequestType(input.requestEnvelope) === "SessionEndedRequest";
+        console.log(SessionEndedRequest.name + "::canHandle::isIntent:", isIntent);
+
+        return isIntent;
+    }
+
+    async handle(input: HandlerInput): Promise<Response> {
+        console.log(SessionEndedRequest.name + "::handle: Request envelope:", input.requestEnvelope);
+
+        return input.responseBuilder
+            .getResponse();
+    }
+}
+
+export class GenericErrorHandler implements ErrorHandler {
+    async canHandle(input: HandlerInput, error: Error): Promise<boolean> {
+        console.log(GenericErrorHandler.name + "::canHandle::input:", input);
+        console.log(GenericErrorHandler.name + "::canHandle::error:", error.message);
+        return true;
+    }
+
+    async handle(input: HandlerInput, error: Error): Promise<Response> {
+        console.log(GenericErrorHandler.name + "::handle: Request envelope:", input.requestEnvelope);
+        console.log(GenericErrorHandler.name + "::handle::error:", error.message);
+
+        return input.responseBuilder
+            .withSimpleCard("Projeto Chiu", "Não entendi. Fale novamente, por favor.")
+            .reprompt("Não entendi. Fale novamente, por favor.")
+            .getResponse();
+    }
+}
+
+export function getLambdaStreamHandler(): LambdaHandler {
+    return SkillBuilders.custom()
+        .addRequestHandlers(
+            new LaunchRequestIntent(),
+            new CurrentNoiseLevelHandler(),
+            new SessionEndedRequest()
+        )
+        .addErrorHandlers(
+            new GenericErrorHandler()
+        )
+        .lambda();
+}
+
+exports.handler = getLambdaStreamHandler()
