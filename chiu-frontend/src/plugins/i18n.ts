bce136cea889990f8f58506e2ccc3be2716d0eff
Lucas <lucasvalenteds@gmail.com>
2020-08-21 00:15:31 -0300

    Translating frontend to English

--- chiu-frontend/src/plugins/i18n.ts
+++ chiu-frontend/src/plugins/i18n.ts
@@ -4,10 +4,11 @@ import VueI18n from "vue-i18n";
 Vue.use(VueI18n);
 
 const i18n = new VueI18n({
-    locale: "br",
-    messages: {
-        br: require("@/i18n/pt-br.json"),
-    },
+  locale: "en",
+  messages: {
+    br: require("@/i18n/pt-br.json"),
+    en: require("@/i18n/en-us.json"),
+  },
 });
 
 export default i18n;
